90e1288900f9fcd59f9d979d1998cedcb621c57b
Marin <marin.clima23@gmail.com>
2024-11-05 19:36:45 +0200

    Task 4: Added the Barista class, a GUI made using the java swing library; refactored the classes, so only the Barista class can access the files, and only after being called by the main class

---
+++ oop-lab3/src/task4/Classes/SyrupCappucino.java
@@ -0,0 +1,34 @@
+package task4.Classes;
+
+class SyrupCappucino extends Cappucino {
+    private SyrupType syrupType;
+    enum SyrupType{
+        MACADAMIA,
+        VANILLA,
+        COCONUT,
+        CARAMEL,
+        CHOCOLATE,
+        POPCORN,
+    }
+
+
+    public SyrupCappucino(SyrupType syrupType, int mlOfMilk, Intensity intensity) {
+        super("Syrup Cappucino", intensity,mlOfMilk);
+        this.syrupType = syrupType;
+    }
+
+    @Override
+    public void printDetails(){
+        super.printDetails();
+        System.out.println("With " + syrupType );
+
+    }
+
+    public static SyrupCappucino makeSyrupCappucino(SyrupType syrupType, int mlOfMilk, Intensity intensity) {
+        SyrupCappucino coffee = new SyrupCappucino(syrupType, mlOfMilk, intensity);
+        coffee.makeCoffee();
+        System.out.println("Adding:" + syrupType + " syrup");
+        System.out.println("Adding " + mlOfMilk + " ml of milk");
+        return coffee;
+    }
+}
---
+++ oop-lab3/src/task4/Main/Main.java
@@ -0,0 +1,10 @@
+package task4.Main;
+
+import task4.Classes.Barista;
+
+public class Main {
+    public static void main(String[] args) {
+        Barista barista = new Barista();
+        barista.showMainMenu();
+    }
+}
